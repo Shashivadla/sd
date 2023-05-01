8279722d884da44ff8d9b78715f639bd552539ac
Shashivadla <120040249+Shashivadla@users.noreply.github.com>
2023-05-01 21:05:16 +0530

    updated lambda

--- src/monthlyInvoiceReconInsert/index.js
+++ src/monthlyInvoiceReconInsert/index.js
@@ -1,213 +1,63 @@
-const get = require("lodash.get");
-const AWS = require("aws-sdk");
-const moment = require("moment-timezone");
-const XLSX = require('xlsx');
-const s3 = new AWS.S3();
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2' });
+const moment = require('moment-timezone');
 const { v4 } = require("uuid");
-const { putItem } = require("../shared/dynamo");
-
-
-
+const sourceTableName = 'salesStagingTable';
+const destinationTableName = 'recon-new';
+const { putItem,getItem } = require("../shared/dynamo")
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
-  console.info("Event: ", JSON.stringify(event));
-  try {
-    await Promise.all(event.Records.map(async (record) => {
 
-      const bucket = event.Records[0].s3.bucket.name;
-      const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-      let tokey ="FFMonthlyInvoice/Outgoing/MI_data"+"_"+v4()
-      // Get the file data from S3
-      const getObjectParams = {
-        Bucket:bucket,
-        Key: key
-        // Bucket: "sales-depot-dev2",
-        // Key: "FFMonthlyInvoice/Incoming/BG Fin report sample_vShare (1).xlsx"
-      };
-      const data = await s3.getObject(getObjectParams).promise();
-    let dataFromExcel = await dataExtract(data)
-      let mapped_Data= await monthlyMapping(dataFromExcel)
-      await moveFile(bucket,key,bucket,tokey)
-    //    console.log(dataFromExcel)
-      console.log(mapped_Data)
-    }));
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    throw "Error while processing t log data.";
-  }
-};
+    try {
+        await Promise.all(event.Records.map(async (record) => {
+          if (record.eventName === "INSERT") {
+              const newImage = AWS.DynamoDB.Converter.unmarshall(
+                record.dynamodb.NewImage
+              );
+              let data = newImage;
+              if(!data.sKey.includes('HEADER')){
+                  let item = data
+                let pKey = data.pKey
+                let key ={
+                    pKey:pKey,
+                    sKey:"HEADER"
+                }
+                let header= await getItem("salesStagingTable",key)
+                let headerItem =header.Item
+                
+                
+                  let  selectedAttributes = {
+                    pKey: item.pKey + '_' + `${JSON.stringify(headerItem.epoch_transaction_date)}`,
+                    sKey: `${item.product_id}` + '_resa_' + v4(),
+                    sku: item.product_id,
+                    return_Qty: item.line_item_qty === '-1' ? '1' : '0',
+                    resa_Gross_Sales: item.line_item_retail_sale,
+                    resa_Qty: item.line_item_qty,
+                    resa_Net: item.line_item_retail_sale,
+                    transaction_Date: headerItem.transaction_date,
+                    // expiration: expiration,
+                    type: "resa"
+                };
+                
+              await putItem("recon-new",selectedAttributes)
+              }
+          }
+        }));
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        throw "Error while processing  data.";
+      }
+    };
 
 
 
-// exports.handler()
-
-
-
-
-
-
-
-
-async function dataExtract(data) {
-  try {
-// const workbook = XLSX.readFile(
-//   "../sampleExcelFiles/BG Fin report sample_vShare (1).xlsx"
-// );
-const workbook = XLSX.read(data.Body);
-const worksheet = workbook.Sheets["DataSet_Order"];
-const options = { header: 1, raw: false, dateNF: "yyyy-mm-dd h:mm:ss" };
-const jsonData = XLSX.utils.sheet_to_json(worksheet, options, {
-  blankRows: false,
-  defval: null,
-});
-let jsonMapped = [];
- jsonData.map((row) => {
-  const headers = jsonData[0];
-  const newRow = {};
-  for (let i = 0; i < headers.length; i++) {
-    const header = headers[i];
-    newRow[header] = row[i] === undefined ? null : row[i];
-  }
-  jsonMapped.push(newRow)
-  
-});  
-console.log("extracting the data from excel and mapping completed")
-return jsonMapped;
-} catch (error) {
-  console.error(`Error: ${error.message}`);
-}
+function getCstDate() {
+    return moment.tz("America/Chicago").format();
 }
 
-
-
-
-async function monthlyMapping(dataFromExcel) {
-  try {
-    let glob=[]
-    for (let i = 0; i < (dataFromExcel.length-1); i++) {
-      let records = dataFromExcel[i+1];
-      let map = {
-        pKey: get(records, "Order Id", null),
-        sKey:
-          get(records, "Order Date", null) +
-          "_" +
-          get(records, "Product Id", null),
-        "order_id": get(records, "Order Id", null),
-        "transaction_type": get(records, "Transaction Type", null),
-        "tenant": get(records, "Tenant", null),
-        "stockpoint_id": get(records, "StockPoint Id", null),
-        "stockpoint_name": get(records, "StockPoint Name", null),
-        "currency": get(records, "Currency", null),
-        "tax_nontax": get(records, "Tax-NonTax", null),
-        "entry_type": get(records, "Entry Type", null),
-        "order_date": get(records, "Order Date", null),
-        "shipment_date": get(records, "Ship. Date", null),
-        "refund_date": get(records, "Refund Date", null),
-        "return_date": get(records, "Return Date", null),
-        "special_payment_date": get(records, "Special Payment Date", null),
-        "adjustment_date": get(records, "Adjustment Date", null),
-        "transaction_date": get(records, "Transaction Date", null),
-        "posting_date": get(records, "Posting Date", null),
-        "invoice_number": get(records, "Invoice No", null),
-        "destination_country": get(records, "Destination Country", null),
-        "destination_state": get(records, "Destination State", null),
-        "ship_to_city": get(records, "Ship-to City", null),
-        "ship_to_zipcode": get(records, "Ship-to ZIP Code", null),
-        "qty": get(records, "Qty", null),
-        "description": get(records, "Description", null),
-        "brand": get(records, "Brand", null),
-        "product_id": get(records, "Product Id", null),
-        "sku": get(records, "SKU", null),
-        "designer_id": get(records, "Designer Id", null),
-        "size": get(records, "Size", null),
-        "season": get(records, "Season", null),
-        "gender": get(records, "Gender", null),
-        "tree_category": get(records, "Tree Category", null),
-        "sub_category": get(records, "Sub Category", null),
-        "reason": get(records, "Reason", null),
-        "reason": get(records, "Reason", null),
-        "reason": get(records, "Reason", null),
-        "sales_price": get(records, "Sales Price", null),
-        "net_sales_price": get(records, "Net Sales Price", null),
-        "promo_codes": get(records, "Promo. Codes", null),
-        "Promo Code Rate": get(records, "Promo Code Rate", null),
-        "tax_rate": get(records, "TAX Rate", null),
-        "total_tax": get(records, "Total TAX", null),
-        "total_items_paid": get(records, "Total Items Paid", null),
-        "order_ship": get(records, "Order Ship.", null),
-        "commission_base": get(records, "Commission Base", null),
-        "effective_commission": get(records, "Effective Commission", null),
-        "effective_commission_rate": get(records,"Effective Commission Rate",null),
-        "software_or_hosting Fee": get(records, "Software/Hosting Fee", null),
-        "special_payment": get(records, "Special Payment", null),
-        "adjustment": get(records, "Adjustment", null),
-        "net_retail_price": get(records, "Net Retail Price", null),
-        "correction": get(records, "Correction", null),
-        "courier_name": get(records, "Courier Name", null),
-        "tracking_code": get(records, "Tracking Code", null),
-        "ship_to_name": get(records, "Ship-to Name", null),
-        "ship_to_phone": get(records, "Ship-to Phone", null),
-        "version": get(records, "Version", null),
-        "us_sales_price": get(records, "US Sales Price", null),
-        "tax_rate_2": get(records, "Tax Rate2", null),
-        "total_tax_2": get(records, "Total Tax2", null),
-        "us_total_items_paid": get(records, "US Total Items Paid", null),
-        "entity": get(records, "Entity", null),
-        "payment": get(records, "Payment", null),
-        "item_duties": get(records, "Item Duties", null),
-        "create_date_time": moment().format(),
-        // "expiration": getUnixExpiration(timenow),
-        "expiration": 365,
-      };
-  glob.push(map)
-      await putItem("sales-depot-monthly-invoice-dev1",map)
-
-    }
-    console.log("mapping completed and inserted to db"+"mapping done according to   "+JSON.stringify(dataFromExcel[0],null,2))
-    return glob
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
+async function getUnixExpiration(lastUpdateTime) {
+    const unixTimestamp = moment(lastUpdateTime).unix();
+    const expirationTimestamp = moment.unix(unixTimestamp).add(2, 'minutes').unix();
+    return expirationTimestamp;
 }
-
-
-async function moveFile(fromBucket, fromKey, toBucket, toKey) {
-  try {
-    // Copy the file to the new location
-    await s3.copyObject({
-      Bucket: toBucket,
-      CopySource: `${fromBucket}/${fromKey}`,
-      Key: toKey
-    }).promise();
-
-    // Delete the original file
-    await s3.deleteObject({
-      Bucket: fromBucket,
-      Key: fromKey
-    }).promise();
-
-    console.log(`Moved ${fromKey} from ${fromBucket} to ${toBucket}/${toKey}`);
-  } catch (err) {
-    console.error(`Error moving file: ${err}`);
-  }
-}
-
-// let getCstDate = (dateString = Date.now()) => {
-//  return moment(new Date(dateString)).tz("America/Chicago");
-// };
-
-// async function getUnixExpiration(timenow) {
-//   let key= {
-//      pKey:"LOOKUP#ADMIN",
-//      sKey:"GLOBAL"
-//    }
-//    let config_data = await getItem(process.env.CONFIG_TABLE,key)
-//    let sd_MonthlyInvoiceDeleteDays = get(config_data.Item, "sd_MonthlyInvoiceDeleteDays", 366)
-//    console.log("sd_MonthlyInvoiceDeleteDays",sd_MonthlyInvoiceDeleteDays)
-//    let unixExpiration = timenow.add(sd_MonthlyInvoiceDeleteDays, "days");
-//     let  epochTimestamp = unixExpiration.unix();
-//     return epochTimestamp
-//  }
-
-
-// monthlyMapping(json);
--- src/resaReconInsert/index.js
+++ src/resaReconInsert/index.js
@@ -4,72 +4,50 @@ const moment = require('moment-timezone');
 const { v4 } = require("uuid");
 const sourceTableName = 'salesStagingTable';
 const destinationTableName = 'recon-new';
-const { putItem } = require("../shared/dynamo")
+const { putItem, } = require("../shared/dynamo")
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
-    console.info("Event: ", JSON.stringify(event));
-    const scanParams = {
-        TableName: sourceTableName,
-        FilterExpression: 'NOT sKey = :skey',
-        ExpressionAttributeValues: {
-            ':skey': 'HEADER',
-        },
-    };
 
     try {
-        const scanResult = await dynamodb.scan(scanParams).promise();
-        const items = scanResult.Items;
+        await Promise.all(event.Records.map(async (record) => {
+          if (record.eventName === "INSERT") {
+              const newImage = AWS.DynamoDB.Converter.unmarshall(
+                record.dynamodb.NewImage
+              );
+              let data = newImage;
+              if(!data.sKey.includes('HEADER')){
+                let pKey = data.pKey
+                let key ={
+                    pKey:pKey,
+                    sKey:"HEADER"
+                }
+                let header= await getItem("salesStagingTable",key)
+                let headerItem =header.Item
+                
+                
+                  const selectedAttributes = {
+                    pKey: item.pKey + '_' + `${JSON.stringify(headerItem.epoch_transaction_date)}`,
+                    sKey: `${item.product_id}` + '_resa_' + v4(),
+                    sku: item.product_id,
+                    return_Qty: item.line_item_qty === '-1' ? '1' : '0',
+                    resa_Gross_Sales: item.line_item_retail_sale,
+                    resa_Qty: item.line_item_qty,
+                    resa_Net: item.line_item_retail_sale,
+                    transaction_Date: headerItem.transaction_date,
+                    expiration: expiration,
+                    type: "resa"
+                };
+              }
+          }
+        }));
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+        throw "Error while processing  data.";
+      }
+    };
 
-        const validAttributes = [];
-        let lastUpdateTime = getCstDate();
-        let expiration = await getUnixExpiration(lastUpdateTime);
 
-        for (const item of items) {
-            // Retrieve the header item for this transaction
-            const headerParams = {
-                TableName: sourceTableName,
-                Key: {
-                    pKey: item.pKey,
-                    sKey: 'HEADER',
-                },
-            };
-            const headerResult = await dynamodb.get(headerParams).promise();
-            const headerItem = headerResult.Item;
-
-            if (!headerItem) {
-                console.warn(`Header item not found for transaction ${item.pKey}`);
-                continue;
-            }
-
-            // Extract attributes you want to insert to destination table
-            const selectedAttributes = {
-                pKey: item.pKey + '_' + `${JSON.stringify(headerItem.epoch_transaction_date)}`,
-                sKey: `${item.product_id}` + '_resa_' + v4(),
-                sku: item.product_id,
-                return_Qty: item.line_item_qty === '-1' ? '1' : '0',
-                resa_Gross_Sales: item.line_item_retail_sale,
-                resa_Qty: item.line_item_qty,
-                resa_Net: item.line_item_retail_sale,
-                transaction_Date: headerItem.transaction_date,
-                expiration: expiration,
-                type: "resa"
-            };
-
-            if (selectedAttributes.pKey !== undefined) {
-                validAttributes.push(selectedAttributes);
-            }
-        }
-
-        for (const item of validAttributes) {
-            await putItem(item, destinationTableName);
-        }
-
-        console.log(`Inserted ${validAttributes.length} items to ${destinationTableName} table.`);
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 function getCstDate() {
     return moment.tz("America/Chicago").format();
